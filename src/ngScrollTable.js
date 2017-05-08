8dae136b894371d64ba87bee2946b90f3457d933
ws72 <song.wang.au@gmail.com>
2017-05-08 22:09:58 +1000

    first commit

---
+++ src/ngScrollTable.js
@@ -0,0 +1,62 @@
+/**
+ * Created by songwang on 8/05/17.
+ */
+
+var mod;
+
+mod = angular.module('ng-scroll-table', []);
+
+mod.directive('scrollTable', [
+    '$rootScope', '$window', '$timeout', function($rootScope, $window, $timeout) {
+        return {
+            link: function(scope, elem, attrs) {
+                var checkWhenEnabled, handler, scrollEnabled;
+                var nScrollHight = 0;
+                var nScrollTop = 0;
+                var nDivHight = elem.height();
+                $window = angular.element(elem);
+                scrollEnabled = true;
+                checkWhenEnabled = false;
+                if (attrs.infiniteScrollDisabled != null) {
+                    scope.$watch(attrs.infiniteScrollDisabled, function(value) {
+                        scrollEnabled = !value;
+                        if (scrollEnabled && checkWhenEnabled) {
+                            checkWhenEnabled = false;
+                            return handler();
+                        }
+                    });
+                }
+                handler = function() {
+                    var elementBottom, remaining, shouldScroll, windowBottom;
+                    windowBottom = $window.height() + $window.scrollTop();
+                    elementBottom = elem.offset().top + elem.height();
+                    nScrollHight = elem[0].scrollHeight;
+                    nScrollTop = elem[0].scrollTop;
+                    shouldScroll = nScrollTop + nDivHight >= nScrollHight && nScrollTop!=0;
+                    if (shouldScroll && scrollEnabled) {
+                        if ($rootScope.$$phase) {
+                            return scope.$eval(attrs.infiniteScroll);
+                        } else {
+                            return scope.$apply(attrs.infiniteScroll);
+                        }
+                    } else if (shouldScroll) {
+                        return checkWhenEnabled = true;
+                    }
+                };
+                $window.on('scroll', handler);
+                scope.$on('$destroy', function() {
+                    return $window.off('scroll', handler);
+                });
+                return $timeout((function() {
+                    if (attrs.infiniteScrollImmediateCheck) {
+                        if (scope.$eval(attrs.infiniteScrollImmediateCheck)) {
+                            return handler();
+                        }
+                    } else {
+                        return handler();
+                    }
+                }), 0);
+            }
+        };
+    }
+]);
